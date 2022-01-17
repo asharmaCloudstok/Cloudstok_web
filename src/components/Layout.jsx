759ac2e345f29040d75d3149dea2e0479c1e57b2
asharmaCloudstok <asharma@cloudstok.com>
2022-01-17 11:40:29 +0530

    changes made

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,11 +4,11 @@ import Header from './header/Header'
 
 const Layout = ({children}) => {
     return (
-        <>
+        <div>
         <Header/>      
         {children}
         <Footer/>      
-        </>
+        </div>
     )
 }
 
