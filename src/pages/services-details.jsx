a9cca6cec425257039a82a3fbb5ea29b72b688ab
asharmaCloudstok <asharma@cloudstok.com>
2022-01-19 17:31:55 +0530

    service details page addeg

--- src/pages/services-details.jsx
+++ src/pages/services-details.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import ServiceAppCard from '../components/servicecard/ServiceAppCard'
+
 
 const ServiceDetails = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+
+        <ServiceAppCard head="Our Expertizes in development makes sure that you get the product you are looking for 
+                       by following right type of development methodology"/>
+      
+        </>
+
     )
 }
 
